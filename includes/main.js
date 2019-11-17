0e32a4bb6cd160c6958a4716da138d4242fb0e13
dpaschal-lf <daniel.paschal@learningfuze.com>
2019-11-16 17:11:36 -0800

    added fun stuff added function to display fun stuff added data for fun stuff basic css for fun stuff

---
+++ includes/main.js
@@ -0,0 +1,119 @@
+
+$(document).ready( startApp );
+
+function startApp(){
+    addFunSections();
+}
+
+var funData = [
+    {
+        title: 'Form Validation',
+        description: 'core js with regex',
+        background: './images/fun/regexform.png',
+        icons: [
+            { classes: 'fab fa-html5' },
+            { classes: 'fab fa-js' },
+            { classes: 'fab fa-css3-alt' },
+            { classes: 'fa fa-eye', url: 'https://jsbin.com/sagayuy/edit?js,output'}
+        ]
+    },
+    {
+        title: 'Stock price lookup',
+        description: 'canvas ajax api',
+        background: './images/fun/ajaxcanvasstocks.png',
+        icons: [
+            { classes: 'fab fa-html5' },
+            { classes: 'fab fa-js' },
+            { classes: 'fa fa-eye', url: 'https://jsbin.com/bawitac/edit?html,js,output'}
+        ]
+    },
+    {
+        title: 'Fade to black',
+        description: 'js css html',
+        background: './images/fun/fadetoblack.png',
+        icons: [
+            { classes: 'fab fa-html5' },
+            { classes: 'fab fa-js' },
+            { classes: 'fa fa-eye', url: 'https://jsbin.com/yomuzop/edit?js,output'}
+        ]
+    },
+    {
+        title: 'syrup pour',
+        description: 'js svg filter-effects',
+        background: './images/fun/syrupeffects.png',
+        icons: [
+            { classes: 'fab fa-js' },
+            { classes: 'fa fa-eye', url: 'https://jsbin.com/sicaroj/edit?output'}
+        ]
+    },
+    {
+        title: 'collapsible folders',
+        description: 'js react-js css',
+        background: './images/fun/collapsiblefolderreact.png',
+        icons: [
+            { classes: 'fab fa-js' },
+            { classes: 'fab fa-react' },
+            { classes: 'fab fa-css3-alt' },
+            { classes: 'fa fa-eye', url: 'https://codepen.io/dpaschal-lf/pen/MzMzdm?editors=0010'}
+        ]
+    },
+    {
+        title: 'collapsible folders',
+        description: 'js react-js css',
+        background: './images/fun/colorlevels.png',
+        icons: [
+            { classes: 'fab fa-js' },
+            { classes: 'fab fa-react' },
+            { classes: 'fab fa-css3-alt' },
+            { classes: 'fa fa-eye', url: 'https://codepen.io/dpaschal-lf/pen/WLNzZa?editors=0010'}
+        ]
+    },
+    {
+        title: 'auto-complete',
+        description: 'js react-js css',
+        background: './images/fun/formautocomplete.png',
+        icons: [
+            { classes: 'fab fa-js' },
+            { classes: 'fab fa-react' },
+            { classes: 'fab fa-css3-alt' },
+            { classes: 'fa fa-eye', url: 'https://codepen.io/dpaschal-lf/pen/REbzPg?editors=0110'}
+        ]
+    },
+    {
+        title: 'nested bars',
+        description: 'js react-js css',
+        background: './images/fun/nestedbars.png',
+        icons: [
+            { classes: 'fab fa-js' },
+            { classes: 'fab fa-react' },
+            { classes: 'fab fa-css3-alt' },
+            { classes: 'fa fa-eye', url: 'https://codesandbox.io/s/nice-bush-kwtu9'}
+        ]
+    }
+]
+
+function addFunSections(){
+    var targetContainer = $("#experimentsAndFun .itemList");
+    for( var dataIndex = 0; dataIndex < funData.length; dataIndex++){
+        var currentData = funData[dataIndex];
+        var item = $("#funItem").clone();
+        item.removeAttr('id');
+        item.find('.background').css('background-image', 'url('+currentData.background+')')
+        item.find('.title').text( currentData.title);
+        item.find('.description').text( currentData.description);
+        for( var iconIndex=0; iconIndex < currentData.icons.length ; iconIndex++){
+            var currentIcon = currentData.icons[iconIndex];
+            var icon = $("<i>").addClass( currentIcon.classes);
+            var container = icon;
+            if(currentIcon.hasOwnProperty('url')){
+                container = $("<a>",{
+                    target: '_blank',
+                    href: currentIcon.url
+                });
+                container.append(icon);
+            }
+            item.find('.info').append(container);
+        }
+        targetContainer.append(item);
+    }
+}
